4b32827e96da4cebe7341c07c159b002d7e74649
Sunil SIngh <chaudharysunil62@gmail.com>
2024-02-04 19:33:16 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:fcm_sample_messaging/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+import 'notificationHandler.dart';
+
+String fcmToken =
+    'AAAAKtki87c:APA91bHpgHiqOMZ0-6qzIJicso5I5pKVyfbZg4roX5s8C3OZZPy1_dJnNdByaO_l3jGcSe_Ma0fzHpnuulk-_y7ip7fH8cZZ_dFUHXsJlcyq32Vu-Ap96GbElzlRT3eaYIfJoAlR0aSq';
+
+@pragma('vm:entry-point')
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  debugPrint('handleBackgroundMessage -message: ${message.data}');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(
+    handleBackgroundMessage, //SHOULD BE TOP LEVEL METHOD ELSE SOME BUGS
+  );
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    firebaseNotification();
+    super.initState();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Homepage(),
+    );
+  }
+
+  void firebaseNotification() {
+    //Sent Notification When App is Running || Background Message is Automatically Sent by Firebase
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      // if (message.notification!.title == "For Live Streaming Chat") {
+
+      try {
+        if (message.data.isNotEmpty) {
+          //  var messageData = json.decode((message.data['body']));
+          //messageData['notificationType'] != null
+          String title = message.data['title'] ?? 'Default Title';
+          String name = message.data['name'] ?? 'Rocky';
+          String id = message.data['id'] ?? '00';
+          String classes = message.data['class'] ?? '01';
+
+          debugPrint('Received Notification:');
+          debugPrint('Title: $title');
+          debugPrint('name: $name');
+          debugPrint('id: $id');
+          debugPrint('classes: $classes');
+
+          String notibody = message.data['body'] ?? {};
+          // String key1 = notibody['key1'] ?? 'defaultValue1';
+          debugPrint('new body is $notibody');
+          NotificationHandler().foregroundNotification(message);
+          await FirebaseMessaging.instance
+              .setForegroundNotificationPresentationOptions(
+                  alert: true, badge: true, sound: true);
+
+          // if (key1 != '') {
+          //   debugPrint('dont show notificaiton key is $key1');
+          // } else {
+
+          // }
+        }
+      } catch (e) {
+        debugPrint('Exception in onMessage:- ${e.toString()}');
+      }
+    });
+    //Perform On Tap Operation On Notification Click when app is in backgroud Or in Kill Mode
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      NotificationHandler().onSelectNotification(json.encode(message.data));
+    });
+  }
+}
---
+++ lib/notificationHandler.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: file_names
+
+import 'dart:convert';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationHandler {
+  final localNotifications = FlutterLocalNotificationsPlugin();
+
+  Future<void> onSelectNotification(String payload) async {
+    Map<dynamic, dynamic> messageData;
+    try {
+      messageData = json.decode(payload);
+      Map<dynamic, dynamic> body = jsonDecode(messageData['body']);
+      debugPrint('notification click body $body');
+      debugPrint('notification click type ${body['notificationType']}');
+      debugPrint('notification key 1 ${body['key1']}');
+      debugPrint('notification key2 ${body['key2']}');
+    } catch (e) {
+      debugPrint(
+        'Exception in onSelectNotification main.dart:- ${e.toString()}',
+      );
+    }
+  }
+
+  Future<void> foregroundNotification(RemoteMessage payload) async {
+    final DarwinInitializationSettings initializationSettingsDarwin =
+        DarwinInitializationSettings(
+      defaultPresentBadge: true,
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+      defaultPresentSound: true,
+      onDidReceiveLocalNotification: (id, title, body, payload) async {
+        debugPrint("object notification call");
+        return;
+      },
+    );
+    AndroidInitializationSettings android =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initialSetting = InitializationSettings(
+        android: android, iOS: initializationSettingsDarwin);
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initialSetting,
+        onDidReceiveNotificationResponse: (_) {
+      NotificationHandler().onSelectNotification(json.encode(payload.data));
+    });
+    AndroidNotificationChannel channel = const AndroidNotificationChannel(
+      'Atroway local notifications',
+      'High Importance Notifications for Atroway',
+      importance: Importance.high,
+    );
+
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      importance: Importance.max,
+      priority: Priority.high,
+      icon: "@mipmap/ic_launcher",
+      playSound: true,
+    );
+    const DarwinNotificationDetails iOSDetails = DarwinNotificationDetails();
+    NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidDetails, iOS: iOSDetails);
+    // global.sp = await SharedPreferences.getInstance();
+    // if (global.sp!.getString("currentUser") != null) {
+
+    // }
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      payload.notification!.title,
+      payload.notification!.body,
+      platformChannelSpecifics,
+      payload: json.encode(payload.data.toString()),
+    );
+  }
+}
